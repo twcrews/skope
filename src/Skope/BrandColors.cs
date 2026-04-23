647a1f9ddaccfdcea6bd8c2645ea9ad544d51a4f
Tommy Crews <twcrews96@gmail.com>
2026-04-23 15:30:03 -0500

    Update themes.

---
+++ src/Skope/BrandColors.cs
@@ -0,0 +1,59 @@
+namespace Skope;
+
+public static class BrandColors
+{
+    // Blue
+    public const string Blue50 = "#E6F1FB";
+    public const string Blue100 = "#B5D4F4";
+    public const string Blue200 = "#85B7EB";
+    public const string Blue400 = "#378ADD";
+    public const string Blue600 = "#185FA5";
+    public const string Blue800 = "#0C447C";
+    public const string Blue900 = "#042C53";
+
+    // Teal
+    public const string Teal50 = "#E1F5EE";
+    public const string Teal100 = "#9FE1CB";
+    public const string Teal200 = "#5DCAA5";
+    public const string Teal400 = "#1D9E75";
+    public const string Teal600 = "#0F6E56";
+    public const string Teal800 = "#085041";
+    public const string Teal900 = "#04342C";
+
+    // Amber
+    public const string Amber50 = "#FAEEDA";
+    public const string Amber100 = "#FAC775";
+    public const string Amber200 = "#EF9F27";
+    public const string Amber400 = "#BA7517";
+    public const string Amber600 = "#854F0B";
+    public const string Amber800 = "#633806";
+    public const string Amber900 = "#412402";
+
+    // Gray (Warm)
+    public const string Gray50 = "#F1EFE8";
+    public const string Gray100 = "#D3D1C7";
+    public const string Gray200 = "#B4B2A9";
+    public const string Gray400 = "#888780";
+    public const string Gray600 = "#5F5E5A";
+    public const string Gray800 = "#444441";
+    public const string Gray900 = "#2C2C2A";
+
+    // Dark mode surfaces (from dark mode palette)
+    public const string DarkBackground = "#0E1621";
+    public const string DarkSurface = "#0A1019";
+    public const string DarkNavBackground = "#070D15";
+    public const string DarkBorder = "#1E2D42";
+    public const string DarkBodyText = "#C8DDF0";
+    public const string DarkMutedText = "#4A6A8A";
+
+    // Alpha variants (base color + opacity suffix)
+    public const string Blue100Alpha30 = "#B5D4F44D";
+    public const string DarkBorderAlpha30 = "#1E2D424D";
+    public const string DarkBodyTextAlpha20 = "#C8DDF033";
+    public const string DarkBackgroundAlpha50 = "#0E162180";
+
+    // Other
+    public const string White = "#FFFFFF";
+    public const string ErrorLight = "#C62828";
+    public const string ErrorDark = "#E53935";
+}

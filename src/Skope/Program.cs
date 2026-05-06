04db6c82a91bb9522dee62dcc15fc12f0338a50a
Tommy Crews <twcrews96@gmail.com>
2026-05-06 10:00:10 -0500

    Implement authentication.

--- src/Skope/Program.cs
+++ src/Skope/Program.cs
@@ -1,6 +1,10 @@
+using Crews.PlanningCenter.Api.Authentication;
+using Crews.PlanningCenter.Api.Extensions;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.EntityFrameworkCore;
 using MudBlazor.Services;
-using Skope;
 using Skope.Components;
 using Skope.Data;
 
@@ -8,9 +12,32 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add MudBlazor services
 builder.Services.AddMudServices();
-builder.Services.AddScoped<MockAuthService>();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = PlanningCenterAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddPlanningCenterAuthentication(options =>
+{
+    options.Scope.Add("api");
+    options.Scope.Add("calendar");
+    options.Scope.Add("check_ins");
+    options.Scope.Add("giving");
+    options.Scope.Add("groups");
+    options.Scope.Add("people");
+    options.Scope.Add("publishing");
+    options.Scope.Add("registrations");
+    options.Scope.Add("services");
+    options.SaveTokens = true;
+});
+
+builder.Services.AddPlanningCenterApi();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -29,8 +56,21 @@ app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages:
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseAntiforgery();
+
+app.MapGet("/login", () => Results.Challenge(
+    new AuthenticationProperties { RedirectUri = "/" },
+    [PlanningCenterAuthenticationDefaults.AuthenticationScheme]))
+    .AllowAnonymous();
+
+app.MapGet("/logout", async (HttpContext context) =>
+{
+    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    return Results.Redirect("/");
+}).RequireAuthorization();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
